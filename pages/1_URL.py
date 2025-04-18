3dfc97a16cfa04159bb03aac441ccb93c4a3d349
walter <wdamiata@gmail.com>
2025-04-18 09:36:11 +0200

    fixing buttons

--- pages/1_URL.py
+++ pages/1_URL.py
@@ -1,16 +1,16 @@
 import streamlit as st
 
+def show():
+    st.title("Inserisci URL Google Sheet")
 
-st.title("Inserisci URL Google Sheet")
+    urlSS = st.text_input("Inserisci l'URL del foglio Google Sheet")
 
-urlSS = st.text_input("Inserisci l'URL del foglio Google Sheet")
+    if 'urlSS' not in st.session_state:
+        st.session_state.urlSS = ""
 
-if 'urlSS' not in st.session_state:
-    st.session_state.urlSS = ""
+    if st.button("Salva URL"):
+        st.session_state.urlSS = urlSS
+        st.success("URL salvato correttamente!")
 
-if st.button("Salva URL"):
-    st.session_state.urlSS = urlSS
-    st.success("URL salvato correttamente!")
-
-if st.button("Avanti ▶️"):
-        st.session_state.page = 'pages/2_Coordinate_e_PDF'
+    if st.button("Avanti ▶️"):
+            st.session_state.page = 'pages/2_Coordinate_e_PDF'
--- pages/3_Email.py
+++ pages/3_Email.py
@@ -1,23 +1,23 @@
 import streamlit as st
+def show():
+    st.title("SDati Email")
 
-st.title("SDati Email")
+    email_mittente = st.text_input("Email mittente")
+    password = st.text_input("Password segreta", type="password")
+    oggetto = st.text_input("Oggetto email")
+    corpo = st.text_area("Corpo email")
 
-email_mittente = st.text_input("Email mittente")
-password = st.text_input("Password segreta", type="password")
-oggetto = st.text_input("Oggetto email")
-corpo = st.text_area("Corpo email")
+    if st.button("Salva dati email"):
+        st.session_state.email_mittente = email_mittente
+        st.session_state.password = password
+        st.session_state.oggetto = oggetto
+        st.session_state.corpo = corpo
+        st.success("Dati salvati!")
 
-if st.button("Salva dati email"):
-    st.session_state.email_mittente = email_mittente
-    st.session_state.password = password
-    st.session_state.oggetto = oggetto
-    st.session_state.corpo = corpo
-    st.success("Dati salvati!")
-
-col1, col2 = st.columns(2)
-with col1:
-    if st.button("◀️ Indietro"):
-        st.session_state.page = 'pages/2_Coordinate_e_PDF'
-with col2:
-    if st.button("Avanti ▶️"):
-        st.session_state.page = 'pages/4_Esecuzione'
+    col1, col2 = st.columns(2)
+    with col1:
+        if st.button("◀️ Indietro"):
+            st.session_state.page = 'pages/2_Coordinate_e_PDF'
+    with col2:
+        if st.button("Avanti ▶️"):
+            st.session_state.page = 'pages/4_Esecuzione'
