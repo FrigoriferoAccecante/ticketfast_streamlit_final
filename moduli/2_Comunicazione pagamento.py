43ca6217d14349bcb8372d939a73773872df8ea5
walter <wdamiata@gmail.com>
2025-05-19 18:41:01 +0200

    different key

--- moduli/2_Comunicazione pagamento.py
+++ moduli/2_Comunicazione pagamento.py
@@ -18,12 +18,12 @@ def show():
 
     col1, col2 = st.columns(2)
     with col1:
-        if st.button("◀️ Indietro",key="btn_indietro"):
+        if st.button("◀️ Indietro",key="btn_indietro2"):
             st.session_state.page_index -= 1
             st.rerun()
 
     with col2:
-        if st.button("Avanti ▶️",key="btn_avanti"):
+        if st.button("Avanti ▶️",key="btn_avanti2"):
             if not accettato:
                 st.warning("Per favore, clicca la casella di conferma.")
             else:
