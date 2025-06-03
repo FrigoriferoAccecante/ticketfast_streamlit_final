d369e4a645d3fb9726b8a27f24509f6b9b328af0
walter <wdamiata@gmail.com>
2025-06-03 10:14:29 +0200

    .

--- token_gen/auth_generate_token.py
+++ token_gen/auth_generate_token.py
@@ -2,7 +2,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
-flow = InstalledAppFlow.from_client_secrets_file('/client_secret.json', SCOPES)
+flow = InstalledAppFlow.from_client_secrets_file('client_secret.json', SCOPES)
 creds = flow.run_local_server(port=0)
 
 # Salva il token per uso futuro
