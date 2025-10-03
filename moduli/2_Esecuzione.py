1eb8790fcc41d04bf65318d39f4f21910cc23f48
FrigoriferoAccecante <43740743+FrigoriferoAccecante@users.noreply.github.com>
2025-10-03 12:27:12 +0000

    modifica biglietto e coordinate

--- moduli/2_Esecuzione.py
+++ moduli/2_Esecuzione.py
@@ -61,8 +61,8 @@ def show():
         sht = gc.open_by_url("https://docs.google.com/spreadsheets/d/1JKjWoutLbN3kE1pDxYttduJSkQtPJV9upWqr0uLeQ3Y/edit?resourcekey=&gid=1686242127#gid=1686242127")
         worksheet = sht.get_worksheet(1)
 
-        x = 1900
-        y = 210
+        x = 2000
+        y = 300
         email_mittente = "picciottiecarusi2@gmail.com"
         password = "xthv czht fcxs zwmn"
         oggetto = "Invito a \'Sia fatta la volontà\'"
