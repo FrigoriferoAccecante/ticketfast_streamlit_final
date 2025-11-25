92c9b189d263693d449c712729184c538968e4c3
FrigoriferoAccecante <43740743+FrigoriferoAccecante@users.noreply.github.com>
2025-11-25 00:54:40 +0000

    .

--- moduli/2_Esecuzione.py
+++ moduli/2_Esecuzione.py
@@ -47,7 +47,7 @@ def salva_dati_excel(nome, cognome, email, data, numero_biglietti_prima, numero_
             df_finale = pd.DataFrame([nuovo_record])
         # Scrivi il DataFrame aggiornato NEL FOGLIO che vuoi, lasciando invariati eventuali altri fogli
         with pd.ExcelWriter(file_path, engine="openpyxl", mode='a' if os.path.exists(file_path) else 'w', if_sheet_exists="replace") as writer:
-            df_finale.to_excel(writer, sheet_name=sheet_name, index=False)
+            df_finale.to_excel(file_path, sheet_name=sheet_name, index=False)
         return True
     except Exception as e:
         st.error(f"Errore nel salvataggio: {str(e)}")
